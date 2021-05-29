71085844df0b5c9d8231ebebaebd937155329365
Yamagishi Kazutoshi <ykzts@desire.sh>
2021-05-29 17:54:55 +0900

    fix(types): respect original types (#340)

---
+++ types/index.d.ts
@@ -0,0 +1,19 @@
+export declare const XMLHttpRequest: {
+  new (): XMLHttpRequest;
+  prototype: XMLHttpRequest;
+  readonly DONE: number;
+  readonly HEADERS_RECEIVED: number;
+  readonly LOADING: number;
+  readonly OPENED: number;
+  readonly UNSENT: number;
+};
+
+export declare const XMLHttpRequestUpload: {
+  new (): XMLHttpRequestUpload;
+  prototype: XMLHttpRequestUpload;
+};
+
+export declare const FormData: {
+  new (form?: HTMLFormElement): FormData;
+  prototype: FormData;
+};
